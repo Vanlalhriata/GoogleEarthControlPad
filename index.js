ffc2debb2498acb12f1446500486c0db5930c15d
Vanlalhriata <vanlalhriata@gmail.com>
2014-10-31 19:33:00 +0530

    Initial commit: Spinning globe

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile('/public/index.html', {"root": __dirname});
+});
+
+app.get('/Control', function(req, res){
+  res.sendFile('/public/control.html', {"root": __dirname});
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
